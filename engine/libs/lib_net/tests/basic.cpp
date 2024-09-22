6e3901696787c9bf97579b8d9f8ae7a16c1028c2
Leo Rentmeister <leo.v.rentmeister@gmail.com>
2024-09-22 18:48:06 +0200

    Fix bad format on test

--- engine/libs/lib_net/tests/basic.cpp
+++ engine/libs/lib_net/tests/basic.cpp
@@ -1,6 +1,6 @@
 
-#include <boost/test/unit_test_suite.hpp>
 #include <boost/test/tools/old/interface.hpp>
+#include <boost/test/unit_test_suite.hpp>
 #define BOOST_TEST_MODULE ECSTest
 
 BOOST_AUTO_TEST_CASE(test_nothing) { BOOST_CHECK(false); }
