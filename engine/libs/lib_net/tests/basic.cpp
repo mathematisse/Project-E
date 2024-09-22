0439c6559cfa930192f6dd2cb8824fd6bb6fe6f3
Leo Rentmeister <leo.v.rentmeister@gmail.com>
2024-09-22 17:42:55 +0200

    Add sample library

---
+++ engine/libs/lib_net/tests/basic.cpp
@@ -0,0 +1,8 @@
+
+#define BOOST_TEST_MODULE ECSTest
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(test_nothing)
+{
+    BOOST_CHECK(false);
+}
---
+++ engine/modules/physics/physics.cpp
@@ -0,0 +1,4 @@
+
+#include "lib_net/lib.hpp"
+
+const int x = MAX_CONNECTIONS;
