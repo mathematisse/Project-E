97434ee2d4d856584b9040de2924e25a3de59a18
Leo Rentmeister <leo.v.rentmeister@gmail.com>
2024-09-23 15:07:49 +0200

    Add tsqueue

--- engine/libs/net/tests/basic.cpp
+++ engine/libs/net/tests/basic.cpp
@@ -1,7 +1,8 @@
-
+// clang-format off
 #include <boost/test/unit_test_suite.hpp>
 #include <boost/test/tools/old/interface.hpp>
 #define BOOST_TEST_MODULE NetTest
 
+// clang-format on
 
 BOOST_AUTO_TEST_CASE(test_nothing) { BOOST_CHECK(false); }
