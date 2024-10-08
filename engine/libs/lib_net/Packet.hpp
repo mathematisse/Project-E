32087570c618fddb31c8f172f91b74f450eb633b
Alexis Hachemi <alexishachemi@gmail.com>
2024-10-08 10:22:02 +0200

    Refactor Packet formatting

--- engine/libs/lib_net/Packet.hpp
+++ engine/libs/lib_net/Packet.hpp
@@ -3,20 +3,17 @@
 #include <cstdint>
 #include <vector>
 
-namespace net
-{
-    struct Packet
-    {
-        struct Header
-        {
-            std::uint32_t type;
-            std::uint16_t size;
-        };
-
-        Header header;
-        std::vector<std::uint8_t> data;
-    
-        std::vector<std::uint8_t> serialize();
-        static Packet deserialize(std::uint32_t type, const std::vector<std::uint8_t> &data);
+namespace net {
+struct Packet {
+    struct Header {
+        std::uint32_t type;
+        std::uint16_t size;
     };
+
+    Header header;
+    std::vector<std::uint8_t> data;
+
+    std::vector<std::uint8_t> serialize();
+    static Packet deserialize(std::uint32_t type, const std::vector<std::uint8_t> &data);
+};
 }
