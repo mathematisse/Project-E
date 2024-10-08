15e25cdf55e25e4400c5728719746fb3bc865a81
Alexis Hachemi <alexishachemi@gmail.com>
2024-10-08 10:15:55 +0200

    Add Context

---
+++ engine/libs/lib_net/Context.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <cstdint>
+
+namespace net {
+struct Context {
+    int max_sd;
+    fd_set readfds;
+    fd_set writefds;
+    sigset_t mask;
+    ssize_t ready_count;
+    bool running;
+};
+};
