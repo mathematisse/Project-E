9e07622f7db348403ff3cb0133a63ccbfebd2e89
Alexis Hachemi <alexishachemi@gmail.com>
2024-10-25 15:43:31 +0200

    Edit cmake to specify required engine modules

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,5 @@ echo "Building the project..."
 set -xe
 git submodule update --init --recursive
 mkdir -p "$ROOT_DIR"/build
-cmake -B "$ROOT_DIR"/build -S "$ROOT_DIR" -DCMAKE_BUILD_TYPE=Debug -Drender_ENABLED=ON -Dspatial2d_ENABLED=ON -Dnetwork_ENABLED=ON -Dcore_ENABLED=ON
+cmake -B "$ROOT_DIR"/build -S "$ROOT_DIR"
 cmake --build "$ROOT_DIR"/build --parallel
