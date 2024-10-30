85d0f5d3de83b91324ff609f48234c434128350d
mathematisse <lucien.pineau@epitech.eu>
2024-10-30 21:12:39 +0100

    Remove files for Project-E

--- rtype/common/lib.cpp
+++
@@ -1,4 +0,0 @@
-
-#include "lib.hpp"
-
-int get_constant() { return rtype::common::SOME_CONSTANT; }
--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,6 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 # Build the project
 echo "Building the project..."
 set -xe
-git submodule update --init --recursive
 mkdir -p "$ROOT_DIR"/build
 cmake -B "$ROOT_DIR"/build -S "$ROOT_DIR"
 cmake --build "$ROOT_DIR"/build --parallel
