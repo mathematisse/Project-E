98897783ebcaaa33b408f75fc2f64002b9f31ec6
mathematisse <lucien.pineau@epitech.eu>
2024-11-02 15:37:05 +0100

    Fix wrong build type on windows

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,5 +1,5 @@
 # Build the project
 echo "Building the project..."
 mkdir -p .\build
-cmake -B .\build -S . -DCMAKE_BUILD_TYPE=Debug
+cmake -B .\build -S . -DCMAKE_BUILD_TYPE=Release
 cmake --build .\build
